af4812fd726e8856d084086c517c4be7e9967494
dideey <bradleyjd32@gmail.com>
2024-10-17 16:55:37 +0300

    completed the assignment by implimenting the traffic light simulation

--- TrafficLightSimulation.ino
+++ TrafficLightSimulation.ino
@@ -6,16 +6,51 @@ const int yellowLightPin = 22; // Yellow LED connected to GPIO 22
 const int greenLightPin = 21;  // Green LED connected to GPIO 21
 const int buttonPin = 19;      // Pedestrian crossing button connected to GPIO 19
 
+bool pedestrianButtonPressed = false; // Flag to indicate if the pedestrian button is pressed
+
 void setup() {
   // Initialize the LED pins as outputs
   // TODO: Set the button pin as INPUT
+  pinMode(redLightPin, OUTPUT);
+  pinMode(yellowLightPin, OUTPUT);
+  pinMode(greenLightPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  // TODO: Implement the traffic light sequence
-  // Use delays for light timing: Red, Yellow, Green
-  
-  // TODO: Implement pedestrian crossing logic
-  // Change light sequence when button is pressed
+  // implement the traffic lights simulation
+  //checks if the button is pressed
+  if (digitalRead(buttonPin) == HIGH) {
+    pedestrianButtonPressed = true;
+  }
+
+  //if the button is pressed, call the pedestrianCrossing function
+  if (pedestrianButtonPressed) {
+    pedestrianCrossing();
+    pedestrianButtonPressed = false;
+  }
+
+  //call the trafficLightSequence function
+  trafficLightSequence();
 }
 
+// Function to control the traffic light sequence, turns the red light on for 5 seconds, then the yellow light for 2 seconds, and finally the green light for 3 seconds
+void trafficLightSequence() {
+  digitalWrite(redLightPin, HIGH);
+  digitalWrite(yellowLightPin, LOW);
+  digitalWrite(greenLightPin, LOW);
+  delay(5000);
+  digitalWrite(yellowLightPin, HIGH);
+  digitalWrite(redLightPin, LOW);
+  delay(2000);
+  digitalWrite(greenLightPin, HIGH);
+  digitalWrite(yellowLightPin, LOW);
+  delay(3000);
+}
+// Function to control the pedestrian crossing, turns the red light on for 10 seconds
+void pedestrianCrossing() {
+  digitalWrite(redLightPin, HIGH);
+  digitalWrite(yellowLightPin, LOW);
+  digitalWrite(greenLightPin, LOW);
+  delay(10000);
+}
